859f34c1da614eedd28baa683fd523baa6280e83
Your Name <you@example.com>
2025-04-01 18:40:01 -0500

    Añadiendo

--- addFuncs/getSkills.sh
+++ addFuncs/getSkills.sh
@@ -1,70 +1,67 @@
 #!/bin/bash
-
 if [ $# -eq 0 ]; then
     echo "Uso: $0 <certificación1> [<certificación2> ...]"
     exit 1
 fi
 source Colors.sh
+
 # Hacer las comparaciones insensibles a mayúsculas y minúsculas
 shopt -s nocasematch
 
 # Archivos temporales
-htb_file="/tmp/cert_htb"
-vuln_file="/tmp/cert_vuln"
-swigger_file="/tmp/cert_swigger"
+htb_file="/tmp/skill_htb"
+vuln_file="/tmp/skill_vuln"
+swigger_file="/tmp/skill_swigger"
 
 # Limpiar archivos anteriores
+
 > "$htb_file"
 > "$vuln_file"
 > "$swigger_file"
 
 # Variables para almacenar datos
+
 machine_name=""
 platform=""
-certs=""
-
+skills=""
 while IFS= read -r line; do
     # Detectar la plataforma
     if [[ $line =~ platform:\ (.*) ]]; then
         platform="${BASH_REMATCH[1]}"
     fi
-
     # Detectar nombre de la máquina
     if [[ $line =~ name:\ (.*) ]]; then
         machine_name="${BASH_REMATCH[1]}"
     fi
-
-    # Detectar certificaciones
-    if [[ $line =~ certification:\ (.*) ]]; then
-        certs="${BASH_REMATCH[1]}"
+    # Detectar skills
+    if [[ $line =~ techniques:\ (.*) ]]; then
+        skills="${BASH_REMATCH[1]}"
         
         match_count=0
         for word in "$@"; do
-            if [[ $certs =~ $word ]]; then
+            if [[ $skills =~ $word ]]; then
                 ((match_count++))
             fi
         done
 
-        # Si todas las certificaciones dadas están en la máquina, la guardamos en la plataforma correspondiente
         if [[ $match_count -eq $# ]]; then
             case "$platform" in
-                "HackTheBox") echo "$machine_name" >> "$htb_file" ;;
+                "HackTheBox") echo -e "$machine_name" >> "$htb_file" ;;
                 "VulnHub") echo -e "$machine_name" >> "$vuln_file" ;;
                 "PortSwigger") echo -e "$machine_name" >> "$swigger_file" ;;
             esac
         fi
     fi
 done < /tmp/bundle.js
-
 get_total(){
+  # Eliminar archivos temporales
   local total_htb=$(wc -l $htb_file | awk '{print $1}')
   local total_swigger=$(wc -l $swigger_file | awk '{print $1}')
   local total_vul=$(wc -l $vuln_file | awk '{print $1}')
-
   echo "$total_htb + $total_vul + $total_swigger" | bc
 }
-
 total_machines=$(get_total)
+
 if [[ $total_machines -ge 1 ]]; then
   echo -e "${bright_cyan}[+]${bright_white} Máquinas encontradas:${bright_magenta} $total_machines${end}"
 fi
@@ -84,6 +81,7 @@ if [[ -s "$swigger_file" ]]; then
   tput setaf 1; /bin/cat $swigger_file | column; tput sgr0; echo
 fi
 
+
 # Mostrar los resultados en formato columnar
 #for file in "$htb_file" "$vuln_file" "$swigger_file"; do
 #    if [[ -s $file ]]; then
@@ -93,5 +91,4 @@ fi
 #        echo ""
 #    fi
 #done
-
 rm -f /tmp/cert_*
--- s4vimachines.sh
+++ s4vimachines.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-[[ $(id -u) -eq 0 ]] && exit 1
-
+ruta=$(realpath $0 | rev | cut -d'/' -f2- | rev)
+cd $ruta
 # Colors
 source Colors.sh
 
@@ -19,14 +19,17 @@ vulnhub_results="/tmp/vulnhub_results"
 exclude_banner=false
 browser='firefox'
 open_browser=false
+show_output_translate=false
+language='en'
 
 function def_handler(){
   echo -e "\n${bright_red}[!] Saliendo del programa...${end}\n\n"
   [[ -f $results  ]] && rm $results
-  [[ -f $TMP_ARCHIVE ]] && rm $PATH_ARCHIVE
+  [[ -f $TMP_ARCHIVE ]] && rm $TMP_ARCHIVE
   [[ -f $vulnhub_results ]] && rm $vulnhub_results
   [[ -f $htb_results ]] && rm $vulnhub_results
-  tput cnorm && exit 1
+  tput cnorm
+  exit 1
 }
 # Ctrl + C
 trap def_handler INT
@@ -63,44 +66,78 @@ function helpPanel(){
   for i in $(seq 1 80); do echo -ne "${bright_red}-"; done; echo -ne "${end}"
     fi
 
-  echo -e "\n${bright_white}Modo de uso: [$0] [PARAMETROS] [ARGUMENTOS]${end}"
-  echo -e "\t${bright_white}-h/--help: Mostrar el manual de ayuda.${end}"
+    echo -e "\n${bright_white}Modo de uso: [$(realpath $0)] [PARAMETROS] [ARGUMENTOS]${end}"
+  echo -e "\t${bright_white}-h(help): Mostrar el manual de ayuda.${end}"
 
   echo -e "\n${bright_white}Actualizaciones y dependencias${end}"
-  echo -e "\t${bright_white}-u/--update: Actualizar dependencias${end}"
-  echo -e "\t${bright_white}-f/--full-install: Buscar e instalar dependencias:${end}"
+  echo -e "\t${bright_white}-u(update): Actualizar dependencias${end}"
 
   echo -e "\n${bright_white}Extras${end}"
-  echo -e "\t${bright_white}-v/--verbose: Activar el modo verbose${end}"
+  echo -e "\t${bright_white}-v(verbose): Activar el modo verbose${end}"
+  echo -e "\t${bright_white}-r(random): Modo de elección aleatorio. El script elegira una máquina al azar por ti.${end}"
+  echo -e "\t${bright_white}-t(translate): Traducir el output a un idioma especifico.${end}"
 
   echo -e "\n${bright_white}Listar máquinas y propiedades.${end}"
-  echo -e "\t${bright_white}-m/--machine: Mostrar las propiedades de una máquina.${end}"
-  echo -e "\t${bright_white}-i/--ip_addr: Mostrar máquinas por la dirección IP.${end}"
-  echo -e "\t${bright_white}-d/--difficulty: Mostrar máquinas por una dificultad dada.${end}"
-  echo -e "\t${bright_white}-o/--osSystem: Mostrar máquinas por un sistema operativo dado.${end}"
-  echo -e "\t${bright_white}-w/--writeup: Mostrar el enlace a una resolución de una máquina${end}"
+  echo -e "\t${bright_white}-m(machine): Mostrar las propiedades de una máquina.${end}"
+  echo -e "\t${bright_white}-i(ip_addr): Mostrar máquinas por la dirección IP.${end}"
+  echo -e "\t${bright_white}-d(difficulty): Mostrar máquinas por una dificultad dada.${end}"
+  echo -e "\t${bright_white}-o(osSystem): Mostrar máquinas por un sistema operativo dado.${end}"
+  echo -e "\t${bright_white}-w(writeup): Mostrar el enlace a la resolución de una máquina${end}"
+  echo -e "\t${bright_white}-s(skill): Listar máquinas por skill${end}"
 
 }
 
 function searchMachine(){
+
+  function helpMachine(){
+  
+    echo -e "\n${bright_cyan}[+]${bright_white} Opciones disponibles:\n"
+    echo -e "\t${bright_magenta}-m${bright_white} Buscar máquinas/s (Ejemplo: $0 -m 'Multimaster')"
+    echo -e "\t${bright_magenta}-t${bright_white} Traducir el output (Ejemplo: $0 -m 'Multimaster' -t 'es')${end}\n"
+    exit 
+  }
+
   machineName="$1"
+  if [[ "$help" == true ]]; then
+    helpMachine
+  fi
+
   if ! cat $PATH_ARCHIVE | grep -i "name: $machineName" &>/dev/null; then
     echo -e "${bright_red}\n[!] Error fatal: Máquina no encontrada ${end}${bg_bright_red}\"$machineName\"\n${end}"
     exit 1
   fi
 
+ 
+  if [[ $show_output_translate == false ]]; then
   output=$(cat $PATH_ARCHIVE| grep -i "name: $machineName" -A 6 -B 1  | sed 's/^ *//g'; echo)
-  echo "$output" | while IFS= read -r line; do
-    first_column=$(echo $line | awk '{print $1}')
-    rest_columns=$(echo $line | cut -d' ' -f2-)
-    if [[ -n "$rest_columns" ]]; then
-      echo -e "${bright_yellow}${first_column}${end} ${bright_white}${rest_columns}${end}" >> $results
-    fi
 
-  done
+    echo "$output" | while IFS= read -r line; do
+      first_column=$(echo $line | awk '{print $1}')
+      rest_columns=$(echo $line | cut -d' ' -f2-)
+      if [[ -n "$rest_columns" ]]; then
+        echo -e "${bright_yellow}${first_column}${end} ${bright_white}${rest_columns}${end}" >> $results
+      fi
 
-  echo -e "\n${bright_cyan}[+]${end} ${bright_white}Maquina encontrada:${end} ${bright_magenta}$machineName${end}${bright_white}, listando sus propiedades:${end}"
-  echo; /bin/cat $results; rm $results; echo
+    done
+
+    echo -e "\n${bright_cyan}[+]${end} ${bright_white}Maquina encontrada:${end} ${bright_magenta}$machineName${end}${bright_white}, listando sus propiedades:${end}"
+    echo; /bin/cat $results | sed 's/--.*//'; rm $results
+  else
+    output=$(cat $PATH_ARCHIVE| grep -i "name: $machineName" -A 6 -B 1  | sed 's/^ *//g' | trans -b -t $language; echo)
+
+    echo "$output" | while IFS= read -r line; do
+      first_column=$(echo $line | awk '{print $1}')
+      rest_columns=$(echo $line | cut -d' ' -f2-)
+      if [[ -n "$rest_columns" ]]; then
+        echo -e "${bright_yellow}${first_column}${end} ${bright_white}${rest_columns}${end}" >> $results
+      fi
+
+    done
+
+    echo -e "\n${bright_cyan}[+]${end} ${bright_white}Maquina encontrada:${end} ${bright_magenta}$machineName${end}${bright_white}, listando sus propiedades:${end}"
+    echo; /bin/cat $results | sed 's/--.*//'; rm $results
+      
+  fi
 }
 
 function searchForIp(){
@@ -130,22 +167,13 @@ function searchDifficulty(){
   difficulty=$(echo $difficulty | tr '[:upper:]' '[:lower:]')
   
   # Validar que la dificultad exista
-  if ! cat $PATH_ARCHIVE | grep -i "$difficulty" &>/dev/null; then
+  if ! cat $PATH_ARCHIVE | grep -i "state: $difficulty" &>/dev/null; then
     echo -e "\n${bright_red}[!] Dificultad no encontrada.${end}\n"
     showAllDifficulty
     exit 1
   fi
   
-  VulnHub=$(cat $PATH_ARCHIVE | grep -i "state: $difficulty" -B 3 | grep "platform: VulnHub" -A 4 | grep "name: *" | sed 's/name://' | sed 's/^ *//' )
-  HackTheBox=$(cat $PATH_ARCHIVE | grep -i "state: $difficulty" -B 3 | grep "platform: HackTheBox" -A 4 | grep "name: *" | sed 's/name://' | sed 's/^ *//' )
-
-  echo "$VulnHub" | while IFS= read -r line; do
-    echo -e "${bright_yellow}${line}${end}"  >> $vulnhub_results
-  done
-
-  echo "$HackTheBox" | while IFS= read -r line; do
-    echo -e "${bright_green}${line}${end}"  >> $htb_results
-  done
+  local total_machines=$(cat $PATH_ARCHIVE | grep -i "state: $difficulty" -B 3  | grep -oP "name: .*" | sed 's/name://' | wc -l)
 
   [[ $help == true ]] && showAllDifficulty && exit 
 
@@ -153,26 +181,20 @@ function searchDifficulty(){
   [[ $difficulty == "medium" ]] && echo -e "${bright_magenta}[+]${end} ${bright_white}Listando máquinas cuya dificultad es${end}${bright_yellow} $difficulty: \n${end}"
   [[ $difficulty == "hard" ]] && echo -e "${bright_magenta}[+]${end} ${bright_white}Listando máquinas cuya dificultad es${end}${bright_cyan} $difficulty: \n${end}"
   [[ $difficulty == "insane" ]] && echo -e "${bright_magenta}[+]${end} ${bright_white}Listando máquinas cuya dificultad es${end}${bright_red} $difficulty: \n${end}"
+  echo -e "[+] Máquinas totales: $total_machines"
 
-  # Para máquinas de HackTheBox
-  if [[ $(sed 's/\x1b\[[0-9;]*m/ /g' $htb_results | tr -d '\n' | wc -c) -gt 20 ]]; then # 720 > 20
-    echo -e "${bright_green}[+]${end} ${bright_white}Máquinas de la plataforma HackTheBox:${end}"
-    echo; cat $htb_results | column
-    echo ""
-  else
-    echo "${bright_black}[*] Máquinas de la plataforma HackTheBox de dificultad: $difficulty no encontradas.${end}" 
+  if cat $PATH_ARCHIVE | grep -i "state: $difficulty" -B 3 | grep -i "platform: HackTheBox" -A 1 | grep -oP "name: .*" | sed 's/name://' &>/dev/null; then
+    echo -e "\n${bright_yellow}[+]${bright_white} Plataforma HackTheBox:${end}"
+    tput setaf 2; echo; cat $PATH_ARCHIVE | grep -i "state: $difficulty" -B 3 | grep -i "platform: HackTheBox" -A 1 | grep -oP "name: .*" | sed 's/name://' | column 
   fi
 
-  # Para máquinas de VulnHub
-  if [[ $(sed 's/\x1b\[[0-9;]*m/ /g' $vulnhub_results | tr -d '\n' | wc -c) -gt 20 ]]; then # 200 > 20
-    echo -e "${bright_yellow}[+]${end} ${bright_white}Máquinas de la plataforma VulnHub:${end}"
-    echo; /bin/cat $vulnhub_results | column
-    echo ""
+
+  if [[ -n $(cat $PATH_ARCHIVE | grep -i "state: $difficulty" -B 3 | grep -i "platform: VulnHub" -A 1 | grep -oP "name: .*" | sed 's/name://' &>/dev/null) ]]; then
+    echo -e "\n${bright_yellow}[+]${bright_white} Plataforma VulnHub:${end}"
+    tput setaf 3; echo; cat $PATH_ARCHIVE | grep -i "state: $difficulty" -B 3 | grep -i "platform: VulnHub" -A 1 | grep -oP "name: .*" | sed 's/name://' | column 
   else
-    echo; echo -e "${bright_black}[*] Máquinas de la plataforma VulnHub de dificultad: $difficulty no encontradas.${end}"
+    echo -e "\n${bright_black}[!] Máquinas de dificultad $difficulty de plataforma VulnHub no encontradas.${end}\n"
   fi
-  
-  rm $vulnhub_results $htb_results
 }
 
 searchOsSystem(){
@@ -183,26 +205,13 @@ searchOsSystem(){
     echo -e "\t${bright_white}Sistemas operativos disponibles:${end} ${bright_cyan}Linux${end} ${bright_white}-${end} ${bright_blue}Windows${end}"
     exit 1
   fi
+  echo -e "\n${bright_green}[+] Listando máquinas de Sistema Operativo: $osSystem\n${end}"
+  echo -e "${bright_cyan}[+]${bright_white} Máquinas de la plataforma HackTheBox:${end}"
+  tput setaf 2; echo; cat $PATH_ARCHIVE | grep -i "os: $osSystem" -B 2 | grep -i "platform: HackTheBox" -A 1 | grep -oP "name: .*" | sed 's/name://' | column; echo
+  
+  echo -e "${bright_cyan}[+]${bright_white} Máquinas de la plataforma VulnHub:${end}"
+  tput setaf 3; echo; cat $PATH_ARCHIVE | grep -i "os: $osSystem" -B 2 | grep -i "platform: VulnHub" -A 1 | grep -oP "name: .*" | sed 's/name://' | column; echo
 
-  HackTheBox=$(cat $PATH_ARCHIVE | grep -i "os: $osSystem" -B 2 | grep "platform: HackTheBox" -A 1 | grep "name: *" | sed 's/name: //' | sed 's/^ *//')
-  VulnHub=$(cat $PATH_ARCHIVE | grep -i "os: $osSystem" -B 2 | grep "platform: VulnHub" -A 1 | grep "name: *" | sed 's/name: //' | sed 's/^ *//')
-
-  echo "$HackTheBox" | while IFS= read -r line; do
-    echo -e "${bright_green}${line}${end}"  >> $htb_results
-  done 
-  
-  echo "$VulnHub" | while IFS= read -r line; do
-    echo -e "${bright_yellow}${line}${end}"  >> $vulnhub_results
-  done
- 
-  [[ $verbose_mode == true ]] && echo -e "${bright_magenta}[+]${end} ${bright_white}Mostrando máquinas cuyo sistema operativo es:${end} ${bright_cyan}$osSystem${end}\n"
-  
-  echo -e "${bright_yellow}[+]${end} ${bright_white}Máquinas de la plataforma VulnHub: ${end}\n"
-  echo; /bin/cat $vulnhub_results | column 
-  
-  echo -e "\n${bright_blue}[+]${end} ${bright_white}Máquinas de HackTheBox: ${end}\n"
-  echo; /bin/cat $htb_results | column
-  rm $htb_results $vulnhub_results
 }
 
 function showLink(){
@@ -216,7 +225,7 @@ function showLink(){
     exit 
   fi
   writeup="$1"
-  writeup=$(echo $writeup | tr '[:upper:]' '[:lower:]')
+  writeup=$(echo "$writeup" | tr '[:upper:]' '[:lower:]')
   if ! cat $PATH_ARCHIVE | grep -i "name: $writeup" -A  6 | grep -i "video: https:" | sed 's/video://' | sed 's/^ *//' &>/dev/null; then
     echo -e "\n[!] Máquina no encontrada: $writeup\n\n"
     exit 1
@@ -254,12 +263,25 @@ function updatefiles(){
     md5_temp=$(md5sum $TMP_ARCHIVE | awk '{print $1}')
     original=$(md5sum $PATH_ARCHIVE | awk '{print $1}')
     if [[ ! $original == $md5_temp ]]; then
-      rm $PATH_ARCHIVE
-      mv $TMP_ARCHIVE $PATH_ARCHIVE
-      [[ $verbose_mode == true ]] && echo -e "${bright_green}[+]${bright_white}  actualizaciones encontradas y finalizadas${end}"
+      if [[ $confirm_act == false ]]; then
+        echo -ne "${bright_green}[+]${bright_white}  Se detectaron algunas actualizaciones, deseas continuar? (Y/n) ${end}" && read -n 1 yes_or_no
+        if [[ $yes_or_no =~ ^[Yy]$ ]]; then
+          rm $PATH_ARCHIVE
+          mv $TMP_ARCHIVE $PATH_ARCHIVE
+          echo -e "\n${bright_green}[+] ${bright_white} Actualizaciones finalizadas.${end}"
+        else
+          echo -e "\n${bright_red}[-]${bright_white} Actualizaciones rechazadas${end}"
+          rm $TMP_ARCHIVE
+          return 1
+        fi
+      else
+          rm $PATH_ARCHIVE
+          mv $TMP_ARCHIVE $PATH_ARCHIVE
+          echo -e "\n${bright_green}[+] ${bright_white} Actualizaciones finalizadas.${end}"  
+      fi
     else
       rm $TMP_ARCHIVE
-      [[ $verbose_mode == true ]] && echo -e "${bright_green}[+]${bright_white}  no se detectaron actualizaciones, estas al día.${end}"
+      echo -e "${bright_green}[+]${bright_white}  No se detectaron actualizaciones, estas al día.${end}"
     fi
   fi
 }
@@ -278,12 +300,82 @@ function Get_cert(){
     echo "./addFuncs/getCertificates.sh ""$certificate" | bash
 }
 
-while getopts 'w:o:b:d:i:m:c:huvyx' arg; do
+function random_machine(){
+  tput civis
+    local value=0
+    while [[ $value -le 100 ]]; do
+        machineName=$(cat $PATH_ARCHIVE | grep -i 'name: .*' | sed 's/name: //' | sed 's/^ *//' | shuf -n 1)
+        echo -ne "\r\033[K${bright_blue}[+]${bright_white} Tu máquina elegida es: ${bright_cyan}\"$machineName\"${end}"
+        ((value++))
+    done
+
+  if ! cat $PATH_ARCHIVE | grep -i "name: $machineName" &>/dev/null; then
+    echo -e "${bright_red}\n[!] Error fatal: Máquina no encontrada ${end}${bg_bright_red}\"$machineName\"\n${end}"
+    exit 1
+  fi
+
+  output=$(cat $PATH_ARCHIVE| grep -iP "name: ${machineName}$" -A 6 -B 1  | sed 's/^ *//g'; echo) 
+  echo "$output" | while IFS= read -r line; do
+    first_column=$(echo $line | awk '{print $1}')
+    rest_columns=$(echo $line | cut -d' ' -f2-)
+    if [[ -n "$rest_columns" ]]; then
+      echo -e "${bright_yellow}${first_column}${end} ${bright_white}${rest_columns}${end}" >> $results
+    fi
+
+  done
+
+  echo -e "\n${bright_cyan}[+]${end} ${bright_white}Maquina encontrada:${end} ${bright_magenta}$machineName${end}${bright_white}, listando sus propiedades:${end}"
+  echo; /bin/cat $results | sed 's/--.*//'; rm $results
+  tput cnorm && exit
+}
+
+function searchPlatform(){
+  platform="$1"
+  
+  if ! cat $PATH_ARCHIVE | grep -i "platform: $platform" &>/dev/null; then
+    echo -e "\n[!] Error fatal, no se enctontro la plataforma: $platform"
+    exit 1
+  fi
+
+  if [[ "$confirm_act" == false ]]; then 
+    if [[ "$platform" =~ ^[Hh] ]]; then
+      tput setaf 2; echo; cat /tmp/bundle.js | grep -i "platform: $platform" -A 1 | grep -oP "name: .*" | sed 's/name://' | column; echo
+    elif [[ "$platform" =~ ^[Pp] ]]; then
+      tput setaf 3 && tput bold; echo; cat /tmp/bundle.js | grep -i "platform: $platform" -A 1 | grep -oP "name: .*" | sed 's/name://' | column; echo
+    else
+       tput setaf 2; echo; cat /tmp/bundle.js | grep -i "platform: $platform" -A 1 | grep -oP "name: .*" | sed 's/name://' | column; echo   
+    fi
+  fi
+
+  if [[ "$confirm_act" == true ]]; then
+    cat $PATH_ARCHIVE | grep -i "platform: $platform" -A 1 | grep -o 'name: .*' | sed 's/name: //' | while read machineName; do
+          searchMachine "$machineName"
+    done
+  fi
+
+}
+
+function searchSkill(){
+  skill="$1"
+  output=$(echo "./addFuncs/getSkills.sh" "$skill" | bash)  # Captura la salida del script
+
+  if [[ -z "$output" ]]; then  # Verifica si la salida no está vacía
+    echo -e "\n${bright_red}[!] No se encontraron matches: $skill${end}"
+    return 1
+  fi
+
+  echo -e "\n${bright_blue}[+]${end} ${bright_white}Listando máquinas que dispongan de skill ${bright_cyan}$skill${bright_white}.${end}\n"
+  echo "./addFuncs/getSkills.sh ""$skill" | bash
+
+}
+
+while getopts 'w:o:b:d:i:m:c:huvyxrp:t:s:' arg; do
   case $arg in
     x) exclude_banner=true;;
     v) verbose_mode=true;;
     y) confirm_act=true;;
     m) machineName=$OPTARG; let parameter_counter+=1;;
+    t) language="$OPTARG"; show_output_translate=true;;
     b) browser="${OPTARG:-firefox}"; open_browser=true;[[ "$browser" == 'default' ]] && browser='firefox';;
     u) let parameter_counter+=2;;
     i) ip_addr=$OPTARG; let parameter_counter+=3;;
@@ -291,6 +383,9 @@ while getopts 'w:o:b:d:i:m:c:huvyx' arg; do
     o) osSystem=$OPTARG; let parameter_counter+=5;;
     w) writeup=$OPTARG; let parameter_counter+=6;;
     c) certificate="$OPTARG"; let parameter_counter+=7;;
+    r) random_machine;;
+    p) platform="$OPTARG"; let parameter_counter+=8;;
+    s) skill="$OPTARG"; let parameter_counter+=9;;
     h) help=true;;
   esac
 done
@@ -310,6 +405,10 @@ elif [[ $parameter_counter -eq 6 ]]; then
   showLink "$writeup"
 elif [[ "$parameter_counter" -eq 7 ]]; then
   Get_cert "$certificate"
+elif [[ "$parameter_counter" -eq 8 ]]; then
+  searchPlatform "$platform"
+elif [[ "$parameter_counter" -eq 9 ]]; then
+  searchSkill "$skill"
 else
   helpPanel
 fi
